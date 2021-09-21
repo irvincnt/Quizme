2dc45fdf86ba2de9b4045cf54203858f97bf2a0f
irvincnt <irvincnt93@gmail.com>
2021-09-21 17:18:04 -0500

    feature(editableElement): Add component EditableElement and styles basic

--- client/src/components/pages/NewSheet.js
+++ client/src/components/pages/NewSheet.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EditableElement from '../helpers/EditableElement'
+
+import '../../styles/pages/sheet.scss'
+import editpencil from '../../asset/icons/paintbrush.svg'
 
 function NewSheet () {
+  const [isEditable, setEdition] = useState(false)
+  const handlerChange = (value) => {
+    console.log('onChange keyUp', value)
+  }
+
+  const handlerEdition = () => {
+    setEdition(!isEditable)
+  }
+
+  const handlerOnBlur = (value) => {
+    setEdition(!isEditable)
+  }
+
   return (
     <div className='container-fluid'>
       <div className='flex align-items-center justify-content-between'>
         <h1>New sheert</h1>
         breadcrumb
       </div>
-      <div className='card'>
-        content
+      <div className='card sheet'>
+        <div className='card-title'>
+          <div className='flex align-items-center gap-5'>
+            <EditableElement
+              onChange={handlerChange}
+              onBlur={handlerOnBlur}
+              disabled={isEditable}
+            >
+              <h4 className='title' id='title-card'>Untitled </h4>
+            </EditableElement>
+            {!isEditable && <img src={editpencil} onClick={handlerEdition} alt='icon editable' />}
+          </div>
+        </div>
       </div>
     </div>
   )
