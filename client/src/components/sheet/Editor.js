078e197135a5d695edd5d79f6d8bee1b5f111a38
irvincnt <irvincnt93@gmail.com>
2021-10-06 12:21:24 -0500

    styles(Editor): Add styles in configuration editor

--- client/src/components/sheet/Editor.js
+++ client/src/components/sheet/Editor.js
@@ -4,6 +4,7 @@ import EditableElement from '../helpers/EditableElement'
 
 import editpencil from '../../asset/icons/paintbrush.svg'
 import '../../styles/pages/sheet.scss'
+import { Check } from 'phosphor-react'
 
 import cardC1 from '../../asset/images/config/card-1c.png'
 import cardC2 from '../../asset/images/config/card-2c.png'
@@ -12,13 +13,16 @@ import cardS1 from '../../asset/images/config/card-style1.png'
 import cardS2 from '../../asset/images/config/card-style2.png'
 import cardS3 from '../../asset/images/config/card-style3.png'
 
+import OneX from '../../asset/images/config/1x.png'
+import TwoX from '../../asset/images/config/2x.png'
+import threeX from '../../asset/images/config/3x.png'
+
 import cardS33Ca from '../../asset/images/config/cardS3/card-s3-3c-a.png'
 import cardS33Cb from '../../asset/images/config/cardS3/card-s3-3c-b.png'
 import cardS33Cc from '../../asset/images/config/cardS3/card-s3-3c-c.png'
 
 function Editor () {
   const [isEditable, setEdition] = useState(false)
-
   const handlerChange = (value) => {
     console.log('onChange keyUp', value)
   }
@@ -46,35 +50,91 @@ function Editor () {
       <div className='config'>
         <div className='items'>
           <p>Columnas</p>
-          <div>
-            <img src={cardC1} alt='one column' className='active' />
-            <img src={cardC2} alt='two column' />
-            <img src={cardC3} alt='three column' />
+          <div className='columns parent'>
+            <div className='children show'>
+              <img src={cardC1} alt='one column' />
+            </div>
+            <div className='children hide'>
+              <img src={cardC2} alt='two column' />
+            </div>
+            <div className='children hide'>
+              <img src={cardC3} alt='three column' />
+            </div>
           </div>
         </div>
         <div className='items'>
           <p>Estilos</p>
-          <div>
-            <img src={cardS1} alt='one column' />
-            <img src={cardS2} alt='two column' className='active' />
-            <img src={cardS3} alt='three column' />
+          <div className='styles parent'>
+            <div className='children hide'>
+              <img src={cardS1} alt='one column' />
+            </div>
+            <div className='children show'>
+              <img src={cardS2} alt='two column' />
+            </div>
+            <div className='children hide'>
+              <img src={cardS3} alt='three column' />
+            </div>
           </div>
         </div>
         <div className='items'>
           <p>Tipos</p>
-          <div>
-            <img src={cardS33Ca} alt='one column' />
-            <img src={cardS33Cb} alt='two column' />
-            <img src={cardS33Cc} alt='three column' className='active' />
+          <div className='types parent'>
+            <div className='children hide'>
+              <img src={cardS33Ca} alt='one column' />
+            </div>
+            <div className='children hide'>
+              <img src={cardS33Cb} alt='two column' />
+            </div>
+            <div className='children show'>
+              <img src={cardS33Cc} alt='three column' />
+            </div>
           </div>
         </div>
         <div className='items'>
           <p>Colores</p>
-          <div />
+          <div className='colors parent-color'>
+            <div className='children-color hide'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle blue' />
+            </div>
+            <div className='children-color show'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle red' />
+            </div>
+            <div className='children-color hide'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle yellow' />
+            </div>
+            <div className='children-color hide'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle green' />
+            </div>
+            <div className='children-color hide'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle purple' />
+            </div>
+            <div className='children-color hide'>
+              <Check size={22} color='#f7f7f8' weight='bold' className='check' />
+              <div className='circle dark' />
+            </div>
+          </div>
         </div>
         <div className='items'>
-          <p>Chartseed size</p>
-          <div />
+          <p>Tamaño</p>
+          <div className='sizes parent'>
+            <div className='size children hide'>
+              <img src={OneX} alt='1x' />
+              <p>1x</p>
+            </div>
+            <div className='size children hide'>
+              <img src={TwoX} alt='2x' />
+              <p>2x</p>
+            </div>
+            <div className='size children show'>
+              <img src={threeX} alt='3x' />
+              <p>3x</p>
+            </div>
+          </div>
         </div>
       </div>
       {/* <ContentEditable
