55c355d8bde4f5ec4a5280ce0c1281e423f07a41
irvincnt <irvincnt93@gmail.com>
2022-01-19 16:13:26 -0600

    build(controlls config): add controlls component basic

--- client/src/components/pages/ConfigSheet.js
+++ client/src/components/pages/ConfigSheet.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Breadcrumb from '../ui/Breadcrumb'
+import { LockKey, LockKeyOpen, Star, Tag, Atom, PlusCircle } from 'phosphor-react'
 
 import '../../styles/pages/sheetConfig.scss'
 import '../../styles/ui/elements.scss'
-import Breadcrumb from '../ui/Breadcrumb'
+import Dropdown from '../ui/Dropdown'
 
 const breadcrumbContent = [
   {
@@ -15,6 +17,29 @@ const breadcrumbContent = [
 ]
 
 function ConfigSheet () {
+  function headDropdown () {
+    return (
+      <div className='item'>
+        <LockKeyOpen size={22} />
+        <span>Público</span>
+      </div>
+    )
+  }
+  function contentDropdown () {
+    return (
+      <>
+        <div className='item'>
+          <LockKey size={22} />
+          <span>Privado</span>
+        </div>
+        <div className='item'>
+          <LockKeyOpen size={22} />
+          <span>Público</span>
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className='configuration container-fluid'>
       <div className='head'>
@@ -22,6 +47,29 @@ function ConfigSheet () {
         <Breadcrumb content={breadcrumbContent} />
       </div>
       <div className='wrapper'>
+        <div className='elements'>
+          <div className='controls'>
+            <Dropdown
+              head={headDropdown()}
+              content={contentDropdown()}
+            />
+            <div className='item'>
+              <Star size={24} color='#f9c10b' weight='fill' />
+            </div>
+            {/* <div className='item'>
+              <Star size={24} color='#f9c10b' weight='fill' />
+            </div> */}
+            <div className='item'>
+              <Atom size={22} />
+              <span>Sección</span>
+            </div>
+            <div className='item'>
+              <Tag size={22} />
+              <span>Tags</span>
+            </div>
+          </div>
+          <button className='btn btn-primary'>Crear</button>
+        </div>
         <div className='sheet'>
           contenido
         </div>
