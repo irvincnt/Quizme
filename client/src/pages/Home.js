68de360575f5388b67cc2c4c09afaa6b142c3cd4
irvincnt <irvincnt93@gmail.com>
2022-04-27 11:12:25 -0500

    remove cheatsheet

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { fetchWithToken } from '../helpers/fetch'
-import PerfilCard from '../components/ui/Perfil-card'
-
-import '../styles/pages/home.scss'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+
 import Navbar from '../components/ui/Navbar'
+import PerfilCard from '../components/ui/Perfil-card'
+import Modal from '../components/ui/Modal'
+import { fetchPromises, fetchWithToken } from '../helpers/fetch'
+import '../styles/pages/home.scss'
 
 function Home () {
   const [stateHome, setHome] = useState({ ok: false, allCheatSheets: [] })
+  const [recharge, setRecharge] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +19,13 @@ function Home () {
       setHome(body)
     }
     fetchData()
-  }, [])
+  }, [recharge])
+
+  const fetchDeleteCheatsheet = async (uid) => {
+    const resp = await fetchPromises('cheatsheet/delete', { uid }, 'DELETE')
+    const respJson = await resp.json()
+    if(respJson.ok) setRecharge(!recharge)
+  }
 
   return (
     <>
@@ -30,16 +38,38 @@ function Home () {
           {
           stateHome.allCheatSheets.map(item => {
             return (
-              <Link key={item.id} className='card cheatsheet' to={`/cheatsheet/${item.id}`}>
-                <span>{item.title}</span>
-                <p>{item.permissions}</p>
-                <p>{item.author.name}</p>
-              </Link>
+              <div key={item.id} className='card cheatsheet'>
+                <Link to={`/cheatsheet/${item.id}`}>
+                  <span>{item.title}</span>
+                  <p>{item.permissions}</p>
+                  <p>{item.author.name}</p>
+                </Link>
+                <DeleteModal item={item} fetchDeleteCheatsheet={fetchDeleteCheatsheet}/>
+              </div>
             )
           })
         }
         </div>
       </div>
+    </>
+  )
+}
+
+function DeleteModal ({item, fetchDeleteCheatsheet }) {
+  const modal = useRef()
+  return (
+    <>
+      <p onClick={() => modal.current.open()}>Eliminar</p>
+      <Modal ref={modal}>
+        <>
+          <p>{`¿Estás seguro de eliminar el cheatsheet ${item.title}?`}</p>
+          <div className='flex justify-content-between'>
+            <button className='btn' onClick={() => modal.current.close()}>Cancelar</button>
+            <button className='btn btn-primary' onClick={() => fetchDeleteCheatsheet(item.id)}>Eliminar apunte</button>
+          </div>
+        </>
+      </Modal>
+      
     </>
   )
 }
